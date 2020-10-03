f37afca640c7b602adc7b138b1447916a36ee67d
Mateusz Jurczak <jurczak.mateusz@gmail.com>
2020-10-04 00:37:36 +0200

    First commit

---
+++ STSW-STLKT01-BLE_SampleApp/BLE_SampleApp/Pin/ble_decode_callback.h
@@ -0,0 +1,52 @@
+/*
+ * ble_decode_callback.h
+ *
+ *  Created on: 3 sty 2020
+ *      Author: Mateusz Jurczak
+ */
+
+#ifndef ST_BLUENRG_MS_INCLUDES_BLE_DECODE_CALLBACK_H_
+#define ST_BLUENRG_MS_INCLUDES_BLE_DECODE_CALLBACK_H_
+
+#include "bluenrg_gatt_aci.h"
+#include "bluenrg_gap.h"
+#include "SpecialTypes.h"
+#include "configuration.h"
+#include "bluenrg_gap_aci.h"
+
+#ifdef BOARD_N64_F4
+	#include "stm32f4xx.h"
+#endif
+#ifdef BOARD_SENSORTILE
+	#include "stm32l4xx.h"
+#endif
+
+void BLE_DECODE_GROUP_RESP(evt_att_read_by_group_resp *cc);
+HAL_StatusTypeDef SET_DECODER_STATE(BLE_State eBLE_State);
+BLE_State GET_DECODER_STATE(void);
+#ifdef BOARD_N64_F4
+void BLE_GET_DEV_DATA(uint8_t DevNo, BLE_DEV_DATA **DevStruct);
+HAL_StatusTypeDef BLE_READ_CHAR(uint16_t ConnHandle, uint8_t *pData, uint8_t len, CharIDData *Characteristic, VoidFuncPointer Callback);
+HAL_StatusTypeDef BLE_READ_MULTIPLE_CHAR(uint16_t ConnHandle, uint8_t *pData, uint8_t len,  uint8_t *Characteristic, uint8_t NoOfhandles, VoidFuncPointer Callback);
+#endif
+#ifdef BOARD_SENSORTILE
+void BLE_GET_DEV_DATA(BLE_DEV_DATA **DevStruct);
+#endif
+//HAL_StatusTypeDef BLE_SET_DISC_DEV_NO(uint8_t DevNo);
+HAL_StatusTypeDef BLE_START_DISC_ALL_DEV_SERVICES(void);
+HAL_StatusTypeDef BLE_START_DISC_ALL_DEV_CHAR(void);
+HAL_StatusTypeDef BLE_ADV_REPORT_CALLBACK(le_advertising_info *cc);
+void BLE_CLEAR_ADV_DATA(void);
+HAL_StatusTypeDef BLE_CONNECT_TO_DEVICE(uint8_t DevNo, BLE_DEVICE_LOCATION ble_dl);
+HAL_StatusTypeDef BLE_START_SCAN(VoidFuncPointer Callback);
+#ifdef BOARD_N64_F4
+HAL_StatusTypeDef BLE_WRITE_CHAR(uint8_t DevNo, ServiceIDData *Service, CharIDData *Characteristic, uint8_t len, uint8_t *pData, VoidFuncPointer Callback);
+#endif
+#ifdef BOARD_SENSORTILE
+HAL_StatusTypeDef BLE_UPDATE_CHAR(ServiceIDData *Service, CharIDData *Characteristic, uint8_t len, uint8_t *pData, VoidFuncPointer Callback);
+#endif
+void BLE_INIT_SPEC(void);
+tBleStatus BLE_ADD_SERVICES(void);
+
+
+#endif /* ST_BLUENRG_MS_INCLUDES_BLE_DECODE_CALLBACK_H_ */
---
+++ STSW-STLKT01-BLE_SampleApp/BOARD_SPEC/BOARD_DEF.h
@@ -0,0 +1,13 @@
+/*
+ * BOARD_DEF.h
+ *
+ *  Created on: Jan 31, 2020
+ *      Author: OMEN
+ */
+
+#ifndef BOARD_SPEC_BOARD_DEF_H_
+#define BOARD_SPEC_BOARD_DEF_H_
+
+#define BOARD_SENSORTILE
+
+#endif /* BOARD_SPEC_BOARD_DEF_H_ */
